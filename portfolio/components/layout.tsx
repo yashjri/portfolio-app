13463dfdebb6ba929a6a8ad1806c3764a5497847
Yash Singh <110559729+yashjri@users.noreply.github.com>
2023-01-13 12:57:40 +0530

    Finish Building Portfolio App

---
+++ portfolio/components/layout.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react'
+
+import Header from './header'
+import Footer from './footer'
+
+interface LayoutProps {
+  children?: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col h-screen justify-between font-content antialiased">
+      
+      <main className="mb-auto">{children}</main>
+     
+    </div>
+  )
+}
+
+export default Layout
